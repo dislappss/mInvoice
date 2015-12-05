e31e00af4c396b2617d7e503237c22e4390ccd1a
DISL <dislappss@gmail.com>
2015-12-05 14:13:57 +0100

    05.12

---
+++ mInvoice/Models/Payment_methodMetadata.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mInvoice.Models
+{
+    [MetadataType(typeof(Payment_methodMetadata))]
+    public partial class Payment_method
+    {
+    }
+
+    public class Payment_methodMetadata
+    {
+        //[ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required]
+        [LocalizedDisplayName("payment_method_code")]
+        public int code { get; set; }
+
+        [Required]
+        [LocalizedDisplayName("payment_method_name")]       
+        public string name { get; set; }
+    }
+
+   
+
+    
+}
