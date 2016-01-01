c59ed4112bb22f62e092c3452141a52579d2289c
DISL <dislappss@gmail.com>
2016-01-01 11:48:55 +0100

    1.1

--- mInvoice/Controllers/HomeController.cs
+++ mInvoice/Controllers/HomeController.cs
@@ -46,7 +46,8 @@ namespace mInvoice.Controllers
             }
             Response.Cookies.Add(cookie);
 
-            return Redirect(Request.UrlReferrer.ToString());
-        }                
+            //return Redirect (Request.UrlReferrer.ToString());
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
